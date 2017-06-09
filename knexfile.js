6d310996b0d93bc9d67352e3ff73e0e8dc0d0285
Sarah Hanson <sarahh.hanson@gmail.com>
2017-06-09 09:49:03 -0500

    knex database connection modified

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/pokemon_data_dev'
+    connection: process.env.DATABASE_URL || 'postgres://localhost/pokemon_data_dev'
   }
 };
